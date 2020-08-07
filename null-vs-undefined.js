b4e45e0130f2c4b08ddf0bb4d46aba5c5c15330b
jasimkasba <saldanodi88@gmail.com>
2020-08-07 10:48:36 +0600

    null vs undefined interview q

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,25 +1,30 @@
 // undefined koyek rokom hote pare
 
-// let pakhi; ata undefined asbe.
+ let pakhi; ata undefined asbe.
 // function a return call na korle undefined asbe 
 
 
-/*function add(num1, num2){
+*function add(num1, num2){
     console.log(num1 + num2);
     return num1 + num2 // sudhu return likhle o undefined asbe
 }
-const result = add(13, 25); */
+const result = add(13, 25); 
 
 // kono parameter ke pass na korle ata by default undefined dekhay.
 
-/* function add(num1, num2){
+* function add(num1, num2){
     console.log(num1, num2);
 }
-const result = add(13); // akhane 2nd peramiter num2 pass kori nai tai ata undefines asbe */
+const result = add(13); // akhane 2nd peramiter num2 pass kori nai tai ata undefines asbe 
 
 // kono object ja thake tar baire kono search korte gale undefined asbe
 
 const pramik = {name: "smart dude", phone: 455477477};
 // akhon amra jodi ai object thake prmikar nam khujte jai tahole undefined asbe coz akhane girl  friend nam nai
-console.log(pramik.gf);
+console.log(pramik.gf);  
+
+// undefined akti reserved word tai kono var value jode undefined nei tahole ata undefined asbe
+
+ let fun = undefined;
+
 
