7c2e919fbc52cf1182a54dd8acc1f2d58b386b0b
jasimkasba <saldanodi88@gmail.com>
2020-08-07 09:57:43 +0600

    true vs false

---
+++ truse-false.js
@@ -0,0 +1,18 @@
+// konta true ar konta false beginerder jonnne kichota confusing
+
+const age = 7;
+
+/*kono songkha ba number  jode value hoy (jemon =7) ar ata  jodi zero hoy tahole false hobe badbaki true hobe,
+akhanel condition age > 0 dile true asbe abar just age dile o hobe */
+
+// string ar belay o same emty string "" false asbe badbaki true result asbe.
+
+// kono var ar value jodi undefined thakee tahole javasricpt atake false dorbe.
+// null hole ata o false asbe 
+//NaN ata false asbe .
+
+if(age){
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
