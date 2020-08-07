49d1644b1176676d1c9c926a3d961f54984e9855
jasimkasba <saldanodi88@gmail.com>
2020-08-07 21:54:49 +0600

    for loop add

--- object-property.js
+++ object-property.js
@@ -5,6 +5,18 @@ const students = [
     {id: 71, name: 'deepjol'}
 ];
 
+// jodi for loop run kore array te nite chai , tahole 1at akta array declare korte hobe tarpor arr.push korte hobe than funtion baire giye console.log korte hobe
+
+let arr = [];
+for(i = 0; i < students.length; i++){
+ const student = students[i];
+ const sName = student.name;
+  const sId = student.id;
+//    console.log(sId);
+    arr.push(sId);
+}
+console.log(arr);
+
 const names = students.map( s => s.name);
 const ids = students.map(s => s.id);
 
@@ -14,4 +26,4 @@ const bigger = students.filter(s => s.id>40);
 // ar jodi find dei tohole prothom ta pabo mane sabnnur pabo
 
 const biggerOne = students.find(s => s.id>40);
-console.log(biggerOne);
+// console.log(biggerOne);
