54d6b28157dc6ec9a741d9c9b328a465c4c2b07d
jasimkasba <saldanodi88@gmail.com>
2020-08-07 11:19:32 +0600

    equal to confution

---
+++ equal.js
@@ -0,0 +1,10 @@
+// double equal (==) sudhu value check kore ar triple equal(===) value and data type aaaa( mane number or string or boolean etc) check kore 
+let first = 2;
+let second = "2";
+if(first=== second){
+    console.log("condition is true");
+}else{
+    console.log("condition is false")
+}
+//akhane (== ) dile condition true asbe abar (===) dile condition false asbe
+//coz == sudhu valu ccheck kore ar akhane 2 value same tai true ase but second var 2 asole akta string .ja asole ak noy.
